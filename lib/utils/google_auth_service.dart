4e4191527c597d67dd2ad65b4798a96bde2130dd
Rafid Khan <rafid.hussainkhan98@gmail.com>
2025-09-02 22:13:21 +0600

    CONFIG: Update GoogleAuthService class

--- lib/utils/google_auth_service.dart
+++ lib/utils/google_auth_service.dart
@@ -1,53 +1,49 @@
+import 'package:check_in/modules/splash/view/splash_screen.dart';
+import 'package:check_in/utils/navigation.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleAuthService {
-  // Private constructor
-  GoogleAuthService._privateConstructor();
+  static GoogleSignInAccount? _currentUser;
 
-  // Single instance
-  static final GoogleAuthService instance =
-      GoogleAuthService._privateConstructor();
-
-  // Firebase Auth instance
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  // GoogleSignIn instance
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
-
-  /// Sign in with Google
-  Future<User?> signInWithGoogle() async {
+  /// Sign in with Google and connect to Firebase
+  static Future<User?> signInWithGoogle() async {
     try {
-      // Trigger the authentication flow
-      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      // 1. Sign in with Google
+      final GoogleSignInAccount? googleUser = await GoogleSignIn.instance
+          .authenticate();
+      if (googleUser == null) return null;
 
-      if (googleUser == null) return null; // User canceled
-
-      // Obtain the auth details
+      // 2. Get Google authentication details
       final GoogleSignInAuthentication googleAuth =
           await googleUser.authentication;
 
-      // Create a credential
-      final credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
+      // 3. Create Firebase credential
+      final OAuthCredential credential = GoogleAuthProvider.credential(
         idToken: googleAuth.idToken,
       );
 
-      // Sign in to Firebase
-      final userCredential = await _auth.signInWithCredential(credential);
+      // 4. Sign in to Firebase
+      final UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithCredential(credential);
+
+      // 5. Update local state
+      _currentUser = googleUser;
+
       return userCredential.user;
-    } catch (e) {
-      print('Google Sign-In Error: $e');
+    } catch (error) {
+      print('Google Sign-In with Firebase Error: $error');
       return null;
     }
   }
 
-  /// Sign out
-  Future<void> signOut() async {
-    await _googleSignIn.signOut();
-    await _auth.signOut();
+  /// Sign out from both Google and Firebase
+  static Future<void> signOut() async {
+    await GoogleSignIn.instance.signOut();
+    await FirebaseAuth.instance.signOut();
+    _currentUser = null;
+    Navigation.pushAndRemoveUntil(const SplashScreen());
   }
 
-  /// Get current user
-  User? get currentUser => _auth.currentUser;
+  // ... other methods if needed ...
 }
